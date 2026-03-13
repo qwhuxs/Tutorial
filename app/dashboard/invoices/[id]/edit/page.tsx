0015d93a69939db406f77bd655ed033822eaf5a2
qwhuxs <vtk241_kai@student.ztu.edu.ua>
2026-03-13 19:09:47 +0200

    feat: create [id]/edit/page.tsx for invoice editing

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,9 +1,10 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
-import { redirect } from 'next/navigation';
+import { notFound } from 'next/navigation';
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const id = params.id;
 
   const [invoice, customers] = await Promise.all([
@@ -12,7 +13,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   ]);
 
   if (!invoice) {
-    redirect('/dashboard/invoices');
+    notFound();
   }
 
   return (
@@ -20,9 +21,14 @@ export default async function Page({ params }: { params: { id: string } }) {
       <Breadcrumbs
         breadcrumbs={[
           { label: 'Invoices', href: '/dashboard/invoices' },
-          { label: 'Edit Invoice', href: `/dashboard/invoices/${id}/edit`, active: true },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
         ]}
       />
+
       <Form invoice={invoice} customers={customers} />
     </main>
   );
